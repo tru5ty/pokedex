555ec0acab5d904a5cc292f56f01821b2f04f1eb
Nathan McGuire <nathanmcguire@Nathans-iMac.gateway>
2015-11-30 09:30:23 +1100

    setup class, add UIcollectionview, setup basic layout

---
+++ pokedex/pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by Nathan McGuire on 30/11/2015.
+//  Copyright © 2015 Nathan McGuire. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+}
