dc5a02da534b32ebfe1ace35b3c7c87684f95ac1
Nathan McGuire <nathanmcguire@Nathans-iMac.gateway>
2015-11-30 15:19:01 +1100

    set up audio, segue, searchbar. set up search filtering.

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Nathan McGuire on 30/11/2015.
+//  Copyright © 2015 Nathan McGuire. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLabel.text = pokemon.name
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
