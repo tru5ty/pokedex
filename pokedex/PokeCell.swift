3ab5382a7207bf3e271766a18ae19544f8cf4a95
Nathan McGuire <nathanmcguire@Nathans-iMac.gateway>
2015-11-30 12:36:00 +1100

    setup custom collectionview cell, delegates & datasource. Set up parsing of CSV file.

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Nathan McGuire on 30/11/2015.
+//  Copyright © 2015 Nathan McGuire. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+        
+    }
+}
